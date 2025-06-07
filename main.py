e67d8a24384dff96eb35680da07540d61e56961a
Alejo Yornet <49294158+alejoy@users.noreply.github.com>
2025-06-06 23:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,7 @@ if not OPENROUTER_API_KEY:
 
 logger.info("✅ Variables de entorno cargadas correctamente")
 
-DESTINATARIOS = {
-    "general": os.getenv("DESTINATARIO_GENERAL")
-}
+DESTINATARIOS = os.getenv("DESTINATARIO_GENERAL")
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
